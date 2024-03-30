44dce5dd134a5ef7c920e12e397471e2c7e92a44
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-30 11:23:48 +0000

    Sum100

---
+++ DotNet/DotNet/Sum100.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Sum100
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("1~100 sum: {0}", sum);
+    }
+}
