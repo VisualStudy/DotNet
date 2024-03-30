232f1bb10d7fc8d63f64ae7b2f2243a091ca6f33
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-30 11:31:50 +0000

    ForEven

---
+++ DotNet/DotNet/ForEven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ForEven
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i; 
+            }
+        }
+
+        Console.WriteLine("1~100 Even sum: {0}", sum);
+    }
+}
