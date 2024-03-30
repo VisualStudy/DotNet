6d65f77d974cf80298546b62c3ba00f33ee1f440
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-30 11:41:29 +0000

    ForIfDemo

---
+++ DotNet/DotNet/ForIfDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ForIfDemo
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) 
+        {
+            if (i % 3 == 0 && i % 4 == 0)
+            { 
+              sum += i; 
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
