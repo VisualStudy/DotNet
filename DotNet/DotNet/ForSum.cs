380bc16486fb3dd08ecd03c808f3953134f5d72b
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-30 10:56:32 +0000

    ForSum

---
+++ DotNet/DotNet/ForSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ForSum
+{
+    static void Main(string[] args)
+    {
+        int n = 3;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) 
+        {
+            sum = sum + i;
+        }
+
+        Console.WriteLine($"{sum}");
+    }
+}
