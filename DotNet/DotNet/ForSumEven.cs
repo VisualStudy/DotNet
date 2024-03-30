a9a8b065fd5148c7cce87b94efb4ecf7f2f274d1
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-30 11:08:56 +0000

    ForSumEven

---
+++ DotNet/DotNet/ForSumEven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ForSumEven
+{
+    static void Main(string[] args)
+    {
+        int n = 5;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"1~{n} sum = {sum}");
+    }
+}
