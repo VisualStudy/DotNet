32ce709fb39bcb6b978f0a42020badfff36482f4
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-31 14:06:09 +0000

    WhileEven

---
+++ DotNet/DotNet/WhileEven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class WhileEven
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        int i = 1;
+        while (i <= 100)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+            i++;
+        }
+        Console.WriteLine($"1~100 even:  {sum}");
+    }
+}
