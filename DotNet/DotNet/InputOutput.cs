f2b17a602ef62a24901b10e1d8b6517c59536106
VisualStudy <sparksoldier@outlook.com>
2024-12-23 17:59:25 +0900

    .

---
+++ DotNet/DotNet/InputOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("당신의 이름: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
+  
